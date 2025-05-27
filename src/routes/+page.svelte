55b004e32bf631e1bed385d6670b381e04a10714
Casper Schipper <casper@perceptor.nl>
2025-05-27 11:10:23 +0200

    Add cleanup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let greeting = 'Hello, World!';
+</script>
+
+<h1>{greeting}</h1>
+<p>This is a SvelteKit application.</p>
+
+<style>
+	h1 {
+		font-size: 3rem;
+		margin-bottom: 1rem;
+	}
+</style>
